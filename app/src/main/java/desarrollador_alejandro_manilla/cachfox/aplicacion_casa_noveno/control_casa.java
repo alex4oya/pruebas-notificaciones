35abd1159c14dabad0cc176a831dca63e07b7066
alex4oya <42258083+alex4oya@users.noreply.github.com>
2018-09-11 21:54:59 -0500

    hola

--- app/src/main/java/desarrollador_alejandro_manilla/cachfox/aplicacion_casa_noveno/control_casa.java
+++ app/src/main/java/desarrollador_alejandro_manilla/cachfox/aplicacion_casa_noveno/control_casa.java
@@ -9,5 +9,7 @@ public class control_casa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_casa);
+
+
     }
 }
